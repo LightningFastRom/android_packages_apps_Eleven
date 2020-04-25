1bcc8781c101b24335ce0737bce0c5634b09fd57
DJABhipHop <castellanos.kevin@rocketmail.com>
2020-04-25 14:25:18 -0400

    Eleven: Get rid use blurred option
    Having a blurred background active player fragment is clean or modern & it's really slow legacy devices + pulse it dates LineageOS & does not fit with new clean and modern redesign.

--- src/org/lineageos/eleven/ui/activities/SlidingPanelActivity.java
+++ src/org/lineageos/eleven/ui/activities/SlidingPanelActivity.java
@@ -64,8 +64,6 @@ public abstract class SlidingPanelActivity extends BaseActivity {
     // this is the blurred image that goes behind the now playing and queue fragments
     private AlbumScrimImage mAlbumScrimImage;
 
-    private boolean mUseBlur;
-
     /**
      * Opens the now playing screen
      */
@@ -100,8 +98,6 @@ public abstract class SlidingPanelActivity extends BaseActivity {
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mUseBlur = PreferenceUtils.getInstance(this).getUseBlur();
-
         mTargetNavigatePanel = Panel.None;
 
         setupFirstPanel();
@@ -196,12 +192,6 @@ public abstract class SlidingPanelActivity extends BaseActivity {
 
     @Override protected void onResume() {
         super.onResume();
-
-        // recreate activity if blur preference has changed to apply changes
-        final boolean useBlur = PreferenceUtils.getInstance(this).getUseBlur();
-        if (mUseBlur != useBlur) {
-            recreate();
-        }
     }
 
     /**
